0e11ab12ed7fa7fc35e8cdff91a03b0322414005
Selcukkorkmaz1 <kselcuk344@gmail.com>
2024-07-28 16:21:09 +0300

    13. Ders Bitti

---
+++ RealEstate_UI/Dtos/ProductDtos/ResultProductDto.cs
@@ -0,0 +1,14 @@
+﻿namespace RealEstate_UI.Dtos.ProductDtos
+{
+    public class ResultProductDto
+    {
+
+            public int productID { get; set; }
+            public string productTitle { get; set; }
+            public decimal productPrice { get; set; }
+            public string productCity { get; set; }
+            public string productDistrict { get; set; }
+            public string categoryName { get; set; }
+       
+    }
+}
--- RealEstate_UI/ViewComponents/HomePage/_DefaultHomePageProductList.cs
+++ RealEstate_UI/ViewComponents/HomePage/_DefaultHomePageProductList.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.ProductDtos;
 
 namespace RealEstate_UI.ViewComponents.HomePage
 {
     public class _DefaultHomePageProductList:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultHomePageProductList(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44327/api/Product/ProductListWithCategory");
+            if(responseMessage.IsSuccessStatusCode)//200 - 299 Arası Değer Döner ise Çalısır
+            {
+                var jsondata=await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultProductDto>>(jsondata);//Json değeri List Dönüştürür
+                return View(values);
+            }
             return View();
         }
     }
