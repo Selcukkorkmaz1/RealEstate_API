d08b893045bea60b46afefa8f3b5432b4b2ff343
Selcukkorkmaz1 <kselcuk344@gmail.com>
2024-08-01 07:55:22 +0300

    Product Admin Tarafı Oluşturuldu(27)

---
+++ RealEstate_UI/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.ProductDtos;
+
+namespace RealEstate_UI.Controllers
+{
+	public class ProductController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public ProductController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client=_httpClientFactory.CreateClient();
+			var responsemessage = await client.GetAsync("https://localhost:44327/api/Product/ProductListWithCategory");
+				if(responsemessage.IsSuccessStatusCode)
+			
+				{
+					var jsondata=await responsemessage.Content.ReadAsStringAsync();
+					var values=JsonConvert.DeserializeObject<List<ResultProductDto>>(jsondata);
+					return View(values);	
+				}
+			return View();
+		}
+		[HttpGet]
+		public async Task<IActionResult>CreateProduct()
+		{
+			return View();
+		}
+	}
+}
---
+++ RealEstate_UI/Dtos/ProductDtos/UpdateProductDto.cs
@@ -0,0 +1,15 @@
+﻿namespace RealEstate_UI.Dtos.ProductDtos
+{
+	public class UpdateProductDto
+	{
+		public int productID { get; set; }
+		public string productTitle { get; set; }
+		public decimal productPrice { get; set; }
+		public string productCity { get; set; }
+		public string productDistrict { get; set; }
+		public string categoryName { get; set; }
+		public string ProductImage { get; set; }
+		public string ProductType { get; set; }
+		public string ProductAddress { get; set; }
+	}
+}
